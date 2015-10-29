e3c8e8dc1096b26444829bd98b0c2a7d9eacc6ab
adaptedbee <adaptedbee@yandex.ru>
2015-10-30 02:16:48 +0300

    Стилизация - меню

--- js/menu.js
+++ js/menu.js
@@ -3,11 +3,13 @@
     var link = document.querySelector(".main-nav__toggle");
     var menu = document.querySelector(".main-nav");
     var close = document.querySelector(".main-nav__toggle--close");
+    var topmenu = document.querySelector(".top-menu");
 
     link.addEventListener("click", function(event) {
         event.preventDefault();
         menu.classList.toggle("main-nav--active");
         link.classList.toggle("main-nav__toggle--close");
+        topmenu.classList.toggle("top-menu--active");
     });
 
 })();
