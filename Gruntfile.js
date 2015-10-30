8626caa31ecfc676c30e4fddf8ad4e853065ae68
adaptedbee <adaptedbee@yandex.ru>
2015-10-30 21:58:11 +0300

    Автоматизация

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,41 +3,129 @@
 module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
 
-  var config = {
-    pkg: grunt.file.readJSON('package.json'),
-
+  grunt.initConfig({
+    clean:{
+      build: ["build"]
+    },
+    copy:{
+      build: {
+        files: [{
+          expand: true,
+          cwd: "",
+          src: [
+            "img/**",
+            "js/**",
+            "index.html"
+          ],
+          dest: "build"
+        }]
+      }
+    },
     sass: {
       style: {
         files: {
-          'css/style.css': 'sass/style.scss'
+          "build/css/style.css": ["sass/style.scss"]
         }
       }
     },
-
+    cmq: {
+      style: {
+        files: {
+          "build/css/style.css": ["build/css/style.css"]
+        }
+      }
+    },
     postcss: {
       options: {
         processors: [
-          require('autoprefixer')({browsers: 'last 2 versions'})
+          require("autoprefixer")({browsers: "last 2 versions"})
         ]
       },
       style: {
-        src: 'css/*.css'
+        src: "build/css/style.css"
       }
     },
-
-    watch: {
+    cssmin: {
+      options: {
+        keepSpecialComments: 0,
+        report: "gzip"
+      },
       style: {
-        files: ['sass/**/*.scss'],
-        tasks: ['sass', 'postcss'],
-        options: {
-          spawn: false,
-          livereload: true
+        files: {
+          "build/css/style.min.css": ["build/css/style.css"]
         }
       }
+    },
+    imagemin: {
+      images: {
+        options: {
+        optimizationLevel: 3
+      },
+      files: [{
+        expand: true,
+        src: ["build/img/**/*.{png,jpg,gif,svg}"]
+      }]
+      }
+    },
+    htmlmin:{
+      options: {
+      removeComments: true,
+      collapseWhitespace: true,
+      collapseBooleanAttributes: true,
+      caseSensitive: true,
+      keepClosingSlash: false
+      },
+      html: {
+        files: {
+          "build/index.min.html": "build/index.html"
+        }
+      }
+    },
+    replace: {
+      build: {
+        options: {
+          patterns: [{
+            match: /[\"\']img\/([^\"\']+)[\"\']/g,
+            replacement: '"/static/img/$1"'
+          },{
+            match: /[\"\']css\/([^\"\']+)[\"\']/g,
+            replacement: '"/static/css/$1"'
+          },{
+            match: /[\"\']js\/([^\"\']+)[\"\']/g,
+            replacement: '"/static/js/$1"'
+          }]
+        },
+        files: [{
+          expand: true,
+          src: [
+            "build/css/style*.css",
+            "build/index*.html"
+          ]
+        }]
+      }
+    },
+    watch: {
+      style: {
+        files: ["sass/**/*.scss"],
+        tasks: ["sass", "cmq", "postcss"]
+      }
+    },
+    csscomb: {
+      style: {
+        expand: true,
+        src: ["sass/**/*.scss"]
+      }
     }
-  };
-
-  config = require('./.gosha')(grunt, config);
-
-  grunt.initConfig(config);
+  });
+  grunt.registerTask("build", [
+    "clean",
+    "copy",
+    "sass",
+    "cmq",
+    "postcss",
+    "cssmin",
+    "imagemin",
+    "htmlmin",
+    "replace"
+  ]);
 };
